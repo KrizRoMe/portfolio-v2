928ac28b1011a094c36c68d6f23d23c5066f0545
KrizRoMe <cmromero1231@gmail.com>
2024-12-05 10:56:01 -0500

    feat(profile): :star: add profile section

--- src/assets/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as MoonSvg } from './layout/header/MoonSvg.astro';
-export { default as SunSvg } from './layout/header/SunSvg.astro';
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
 export { default as Header } from './layout/header/Header.astro';
+export { default as MoonIcon } from './layout/header/icons/MoonIcon.astro';
+export { default as SunIcon } from './layout/header/icons/SunIcon.astro';
