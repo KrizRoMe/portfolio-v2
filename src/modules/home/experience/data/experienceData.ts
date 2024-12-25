19c8c3c5939a6cbf24b7ee7b124a227086d3b145
KrizRoMe <cmromero1231@gmail.com>
2024-12-25 11:06:06 -0500

    feat(experience): :star: update experience information

---
+++ src/modules/home/experience/data/experienceData.ts
@@ -0,0 +1,63 @@
+export const experienceData = [
+	{
+		title: 'Desarrollador de Software Semi Senior',
+		company: 'Datakraft',
+		startDate: '2024',
+		endDate: 'Presente',
+		descriptionList: [
+			'Automatización de CI/CD, mejorando la eficiencia en un 30 %.',
+			'Integración de mensajería en tiempo real con la API de WhatsApp Business.',
+			'Refuerzo de seguridad en servidor y cookies con encabezados HTTP y atributos Secure.',
+			'Sistema de inventario completo desde entrada hasta salida de productos (React, DRF, PostgreSQL).',
+			'Notificaciones por SMS y autenticación de usuarios (Twilio, NextAuth).',
+		],
+	},
+	{
+		title: 'Desarrollador de Software Semi Senior',
+		company: 'Datakraft',
+		startDate: '2024',
+		endDate: 'Presente',
+		descriptionList: [
+			'Automatización de CI/CD, mejorando la eficiencia en un 30 %.',
+			'Integración de mensajería en tiempo real con la API de WhatsApp Business.',
+			'Refuerzo de seguridad en servidor y cookies con encabezados HTTP y atributos Secure.',
+			'Sistema de inventario completo desde entrada hasta salida de productos (React, DRF, PostgreSQL).',
+			'Notificaciones por SMS y autenticación de usuarios (Twilio, NextAuth).',
+		],
+	},
+	{
+		title: 'Desarrollador de Software Semi Senior',
+		company: 'Datakraft',
+		startDate: '2023',
+		endDate: '2024',
+		descriptionList: [
+			'Despliegues en producción con SSH (Docker, Git).',
+			'Corrección de vulnerabilidades críticas como IDOR, reduciendo riesgos en un 40 %.',
+			'Funcionalidades ETL para optimizar BI en una empresa minera (SQL Server, Python).',
+			'Lideré sesiones regulares de asignación y revisión de tareas (PRs) a 3 desarrolladores (Github). ',
+		],
+	},
+	{
+		title: 'Desarrollador de Software Semi Senior',
+		company: 'Datakraft',
+		startDate: '2022',
+		endDate: '2023',
+		descriptionList: [
+			'Landing page para Datakraft, aumentando clientes en un 30 % (React, TailwindCSS).',
+			'Solución automatizada para generación de pruebas y respuestas (OpenAI, PostgreSQL).',
+			'Capacitación al personal médico en procesos de admisión y atención.',
+		],
+	},
+	{
+		title: 'Desarrollador de Software Semi Senior',
+		company: 'Datakraft',
+		startDate: '2021',
+		endDate: '2022',
+		descriptionList: [
+			'Funcionalidad de compartir decks y páginas con usuarios anónimos (Django, JavaScript, Ajax).',
+			'Interfaz de sala de espera en ERP clínico, reduciendo tiempos en un 25 % (Next.js, TailwindCSS).',
+			'Migración de ERP clínico a una versión reciente de Python, mejorando compatibilidad.',
+			'Manejo de variables de entorno para mayor seguridad (MySQL).',
+		],
+	},
+];
---
+++ src/modules/home/experience/interfaces/experieceInterface.ts
@@ -0,0 +1,7 @@
+export interface ExperienceInterface {
+	title: string;
+	company: string;
+	startDate: string;
+	endDate: string;
+	descriptionList: string[];
+}
