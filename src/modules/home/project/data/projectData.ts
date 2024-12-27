fc0b6dc735eb9d99cf4cd75e24af8d00cd63af21
KrizRoMe <cmromero1231@gmail.com>
2024-12-27 17:25:46 -0500

    feat(project): :star: update project information

---
+++ src/modules/home/project/data/projectData.ts
@@ -0,0 +1,26 @@
+export const projectData = [
+	{
+		name: 'Perfigit',
+		description:
+			'App web enfocada a generar perfiles README de GitHub mediante IA.',
+		image: '/600x400.svg',
+	},
+	{
+		name: 'Intiguard',
+		description:
+			'App móvil enfocada en mejorar la seguridad pública con gestión de incidentes y comunicación rápida con autoridades.',
+
+		image: '/600x400.svg',
+	},
+	{
+		name: 'Academic Risk',
+		description:
+			'App web enfocada a reducir el índice de estudiantes en riesgo académico.',
+		image: '/600x400.svg',
+	},
+	{
+		name: 'Cuartos Romero',
+		description: 'App web enfocada a la gestión de alquiler de habitaciones.',
+		image: '/600x400.svg',
+	},
+];
