70e73495945b4233b73ce7d5114b61c552a7abf2
KrizRoMe <cmromero1231@gmail.com>
2025-07-31 12:37:58 -0500

    feat(project): :star: add tee-craft project

--- src/modules/home/project/data/projectData.ts
+++ src/modules/home/project/data/projectData.ts
@@ -5,26 +5,27 @@ export const projectData = [
 			'App web enfocada a generar perfiles README de GitHub mediante IA.',
 		image: '/project/image/image-perfigit.webp',
 		href: 'https://perfigit.lat/',
-		technologies: ['React', 'Next.js', 'Typescript', 'Tailwind'],
+		technologies: [
+			'React',
+			'Next.js',
+			'TypeScript',
+			'Tailwind CSS',
+			'Vercel AI SDK',
+			'OpenAI API',
+		],
 	},
-	// {
-	// 	name: 'Intiguard',
-	// 	description:
-	// 		'App móvil enfocada en mejorar la seguridad pública con gestión de incidentes y comunicación rápida con autoridades.',
-	// 	image: '/project/image/image-intiguard.webp',
-	// 	href: 'http://sv-s0d3ygzevg.cloud.elastika.pe/landing-page',
-	// },
-	// {
-	// 	name: 'Academic Risk',
-	// 	description:
-	// 		'App web enfocada a reducir el índice de estudiantes en riesgo académico.',
-	// 	image: '/project/image/image-academicrisk.webp',
-	// 	href: 'https://academic-risk.onrender.com/',
-	// },
-	// {
-	// 	name: 'Cuartos Romero',
-	// 	description: 'App web enfocada a la gestión de alquiler de habitaciones.',
-	// 	image: '/project/image/image-cuartosromero.webp',
-	// 	href: 'https://cuartos-romero-dashboard-pfok11ay3-kriztofer3103s-projects.vercel.app/login',
-	// },
+	{
+		name: 'Tee Craft',
+		description: 'App web enfocada  en personalizar camisetas en 3D',
+		image: '/project/image/image-tee-craft.webp',
+		href: 'https://teecraft.lat/',
+		technologies: [
+			'React',
+			'Next.js',
+			'TypeScript',
+			'Tailwind CSS',
+			'React Three Fiber',
+			'Three.js',
+		],
+	},
 ];
